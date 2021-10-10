2ec6eed0b6c731a76bae48f630c0dbc95dd911e1
jesanhe <jerosancho@gmail.com>
2021-10-10 19:35:48 +0200

    feat(exam): added exam module

---
+++ src/app/pages/exam/exam.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExamComponent } from './exam.component';
+
+
+
+@NgModule({
+  declarations: [
+    ExamComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExamModule { }
