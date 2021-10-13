e9ea92990ca9b5120717831cc0d92bd32c1fda7f
jesanhe <jerosancho@gmail.com>
2021-10-13 09:19:40 +0200

    feat(exam): added exam main page

--- src/app/pages/exam/exam.component.ts
+++ src/app/pages/exam/exam.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
+import { Question } from 'src/app/core/models';
+import { AppFacadeService } from 'src/app/store/app-facade.service';
+import { ExamFacadeService } from 'src/app/store/exam/exam-facade.service';
 
 @Component({
   selector: 'app-exam',
   templateUrl: './exam.component.html',
-  styleUrls: ['./exam.component.scss']
+  styleUrls: ['./exam.component.scss'],
+  host: {
+    class: 'flex flex-col gap-4',
+  },
 })
 export class ExamComponent implements OnInit {
+  questions$!: Observable<string[]>;
 
-  constructor() { }
+  constructor(private appStore: AppFacadeService) {}
 
   ngOnInit(): void {
+    this.questions$ = this.appStore.getQuestionsInExamIds();
   }
-
 }
--- src/app/pages/exam/exam.module.ts
+++ src/app/pages/exam/exam.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExamComponent } from './exam.component';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionModule } from 'src/app/components/question/question.module';
+import { PieChartModule } from 'src/app/components/pie-chart/pie-chart.module';
+import { QuestionNavModule } from 'src/app/components/question-nav/question-nav.module';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ExamComponent,
+  },
+  {
+    path: '**',
+    component: ExamComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    ExamComponent
-  ],
+  declarations: [ExamComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+    QuestionModule,
+    PieChartModule,
+    QuestionNavModule,
+  ],
 })
-export class ExamModule { }
+export class ExamModule {}
